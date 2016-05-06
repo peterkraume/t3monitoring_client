0490d39ec51c09f3b911034e02efaacce05bf1d3
Andreas Hirsch <hirscha@gmx.de>
2016-05-06 12:31:10 +0200

    TYPO3 6.x compatibility

--- Classes/Provider/StatusReportProvider.php
+++ Classes/Provider/StatusReportProvider.php
@@ -63,7 +63,9 @@ class StatusReportProvider implements DataProviderInterface
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['configuration'][] = 'TYPO3\\CMS\\Reports\\Report\\Status\\ConfigurationStatus';
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['fal'][] = 'TYPO3\\CMS\\Reports\\Report\\Status\\FalStatus';
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['typo3'][] = 'TYPO3\\CMS\\Install\\Report\\InstallStatusReport';
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['security'][] = 'TYPO3\\CMS\\Install\\Report\\SecurityStatusReport';
+        if (\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('7.0')) {
+            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['security'][] = 'TYPO3\\CMS\\Install\\Report\\SecurityStatusReport';
+        }
     }
 
     /**
