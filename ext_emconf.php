dfae9fa55971b6ef5fe318e4a2d340fe7b986193
Peter Kraume <peter.kraume@gmx.de>
2018-10-25 22:40:55 +0200

    [TASK] raise maximum TYPO3 version to 9.5 and set version number to 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-9.3.99',
+            'typo3' => '6.2.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
